d5c70369e644b3800e7acc94eba57047520fc187
Ludovic Gardy <ludovic.gardy@cnrs.fr>
2022-01-27 23:09:10 +0100

    micro ID parameter is now available

--- open_GUI.py
+++ open_GUI.py
@@ -16,7 +16,8 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import QCoreApplication
 import time
 
-sys.path.append(r"G:\GardyL\Data_storage\EPIFAR_storage\BIDS_data\derivatives\Neuralynx_to_BIDSlike")
+#sys.path.append(r"G:\GardyL\Data_storage\EPIFAR_storage\BIDS_data\derivatives\Neuralynx_to_BIDSlike")
+sys.path.append(r"C:\Users\GARDy\Desktop\Neuralynx_to_BIDSlike")
 from create_neuralynx_BIDSlike import path_to_BIDSlikepath
 
 class empty_win(QWidget):
@@ -32,10 +33,10 @@ class empty_win(QWidget):
         left = 200
         top = 100
         width = 600
-        height = 150
+        height = 600
 
         self.setGeometry(left, top, width, height)
-        self.setWindowTitle('BIDS like architecture maker')
+        self.setWindowTitle('BIDS-like architecture creator')
 
         ### BIDS naming frame
         BIDSnaming_frame = QFrame()
@@ -58,8 +59,9 @@ class empty_win(QWidget):
         self.run_edit = QLineEdit()
         self.run_edit.setText("00")
 
-        json_dict = json.load(open("task_names.json"))
+        json_dict = json.load(open("config_file.json"))
         possible_tasknames = json_dict["possible_tasknames"]
+        possible_ext = json_dict["possible_ext"]
         possible_tasknames.append("Other")
 
         task_label = QLabel("  Task-")
@@ -82,31 +84,55 @@ class empty_win(QWidget):
         input_format_layout = QHBoxLayout()
         input_format_frame.setLayout(input_format_layout)
         input_format_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        input_format_frame.setFixedHeight(30)
+        input_format_frame.setFixedHeight(50)
         input_format_layout.setContentsMargins(0, 0, 0, 0)
         input_format_layout.setSpacing(0)
 
-        self.input_format_label = QLabel("Input format: ")
+        ##- Input data type subframe 1
+        input_format_subframe1 = QFrame()
+        input_format_sublayout1 = QHBoxLayout()
+        input_format_subframe1.setLayout(input_format_sublayout1)
+        input_format_subframe1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        input_format_subframe1.setFixedHeight(30)
+        input_format_sublayout1.setContentsMargins(0, 0, 0, 0)
+        input_format_sublayout1.setSpacing(0)
 
-        self.input_cbox_ncs = QCheckBox(".ncs")
-        self.input_cbox_ncs.setStatusTip("--")
-        self.input_cbox_ncs.setChecked(False)
-        self.input_cbox_ncs.stateChanged.connect(self.input_cbox_fun)
+        input_format_label = QLabel("Input format: ")
+        self.ext_ComboBox = QComboBox()
+        [self.ext_ComboBox.addItem(ext) for ext in possible_ext]
+        self.ext_ComboBox.setCurrentIndex(1)
+        self.ext_ComboBox.currentIndexChanged.connect(self.ext_ComboBox_fun)
+        self.ext_ComboBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.input_cbox_nrd = QCheckBox(".nrd")
-        self.input_cbox_nrd.setStatusTip("--")
-        self.input_cbox_nrd.setChecked(False)
-        self.input_cbox_nrd.stateChanged.connect(self.input_cbox_fun)
+        input_format_sublayout1.addWidget(input_format_label)
+        input_format_sublayout1.addWidget(self.ext_ComboBox)
 
-        self.input_cbox_trc = QCheckBox(".trc")
-        self.input_cbox_trc.setStatusTip("--")
-        self.input_cbox_trc.setChecked(False)
-        self.input_cbox_trc.stateChanged.connect(self.input_cbox_fun)
+        ##- Input data type subframe 2
+        input_format_subframe2 = QFrame()
+        input_format_sublayout2 = QHBoxLayout()
+        input_format_subframe2.setLayout(input_format_sublayout2)
+        input_format_subframe2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        input_format_subframe2.setFixedHeight(30)
+        input_format_sublayout2.setContentsMargins(0, 0, 0, 0)
+        input_format_sublayout2.setSpacing(0)
 
-        input_format_layout.addWidget(self.input_format_label)
-        input_format_layout.addWidget(self.input_cbox_ncs)
-        input_format_layout.addWidget(self.input_cbox_nrd)
-        input_format_layout.addWidget(self.input_cbox_trc)
+        microID_label = QLabel("                micro ID: ")
+        self.microID_edit = QLineEdit("")
+        self.microID_edit.setText("t")
+        self.microID_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        microID_infoButton = QPushButton("")
+        microID_infoButton.clicked.connect(self.microID_infoButton_fun)
+        microID_infoButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        microID_infoButton.setFixedWidth(50)
+        microID_infoButton.setIcon(self.style().standardIcon(QStyle.SP_MessageBoxInformation))
+
+        input_format_sublayout2.addWidget(microID_label)
+        input_format_sublayout2.addWidget(self.microID_edit)
+        input_format_sublayout2.addWidget(microID_infoButton)
+
+        input_format_layout.addWidget(input_format_subframe1)
+        input_format_layout.addWidget(input_format_subframe2)
 
         ### Input data path frame
         input_path_frame = QFrame()
@@ -134,9 +160,10 @@ class empty_win(QWidget):
 
         self.input_ext_edit = QLineEdit()
         self.input_ext_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.input_ext_edit.setText("")
+        self.input_ext_edit.setText(self.ext_ComboBox.currentText())
         self.input_ext_edit.setFixedWidth(50)
-        self.input_ext_edit.setStyleSheet("background-color: lightgray")
+        #self.input_ext_edit.setStyleSheet("background-color: lightgray")
+        self.input_ext_edit.setStyleSheet("background: #636363 ;color: #ffffff;")
 
         input_path_layout.addWidget(input_infoButton)
         input_path_layout.addWidget(input_browseButton)
@@ -199,10 +226,20 @@ class empty_win(QWidget):
         validation_layout.addWidget(button_OK)
 
         # Set global layout
+        title1 = QLabel("DATA STRUCTURE")
+        title1.setAlignment(QtCore.Qt.AlignCenter)
+        title1.setStyleSheet("background: #636363 ;border: 2px solid #000000;font-weight: bold;font-size: 18pt;color: #ffffff;")
+
+        title2 = QLabel("DATA PATH")
+        title2.setAlignment(QtCore.Qt.AlignCenter)
+        title2.setStyleSheet("background: #636363 ;border: 2px solid #000000;font-weight: bold;font-size: 18pt;color: #ffffff;")
+
         GLOBAL_layout = QVBoxLayout(self)
 
+        GLOBAL_layout.addWidget(title1)
         GLOBAL_layout.addWidget(BIDSnaming_frame)
         GLOBAL_layout.addWidget(input_format_frame)
+        GLOBAL_layout.addWidget(title2)
         GLOBAL_layout.addWidget(input_path_frame)
         GLOBAL_layout.addWidget(output_path_frame)
         GLOBAL_layout.addWidget(validation_frame)
@@ -211,39 +248,29 @@ class empty_win(QWidget):
         if self.taskname_ComboBox.currentText() == "Other":
             error_dialog.setWindowTitle("Information")
             error_dialog = QErrorMessage()
-            error_dialog.showMessage('If you want to add a new task name, you need to add it in the "task_names.json" file, which you can find in the root folder of this program.')            
+            error_dialog.showMessage('If you want to add a new task name, you need to add it in the "task_names.json" file, which you can find in the root folder of this program.')
             error_dialog.exec()
 
-    def input_cbox_fun(self, callback):
+    def ext_ComboBox_fun(self, callback):
+        self.input_ext_edit.setText(self.ext_ComboBox.currentText())
 
-        cbox_ncs_state = self.input_cbox_ncs.isChecked()
-        cbox_nrd_state = self.input_cbox_nrd.isChecked()
-        cbox_trc_state = self.input_cbox_trc.isChecked()
-
-        if int(cbox_ncs_state) + int(cbox_nrd_state) + int(cbox_trc_state) > 1:
-            self.input_cbox_ncs.setChecked(False)
-            self.input_cbox_nrd.setChecked(False)
-            self.input_cbox_trc.setChecked(False)
-            self.input_ext_edit.setText("")
-        else:
-            if self.input_cbox_ncs.isChecked():
-                self.input_ext_edit.setText(".ncs")
-            elif self.input_cbox_nrd.isChecked():
-                self.input_ext_edit.setText(".nrd")
-            elif self.input_cbox_trc.isChecked():
-                self.input_ext_edit.setText(".trc")
+    def microID_infoButton_fun(self):
+        microID_infoButton_dialog = QErrorMessage()
+        microID_infoButton_dialog.setWindowTitle("Information about the 'micro ID' parameter")
+        microID_infoButton_dialog.showMessage('This parameter is only necessary if the dataset include micro-electrodes. If that is the case, enter the letter(s) used to differenciate micro-channels from macro-channels.')
+        microID_infoButton_dialog.exec()
 
     def input_infoButton_fun(self):
         input_infoButton_dialog = QErrorMessage()
         input_infoButton_dialog.setWindowTitle("Input path information")
-        input_infoButton_dialog.showMessage('Search or paste the path to the data you want to turn into a BIDS-like. For .ncs choose the folder, for .trc and .nrd choose the file.')            
+        input_infoButton_dialog.showMessage('Search or paste the path to the data you want to turn into a BIDS-like. For .ncs choose the folder, for .trc and .nrd choose the file.')
         input_infoButton_dialog.exec()
 
     def input_browseButton_fun(self):
         selected_path = ""
-        if self.input_cbox_trc.isChecked() or self.input_cbox_nrd.isChecked():
-            selected_path = QFileDialog.getOpenFileName(None, "Select an EEG file:".format(True), fpath, "EEG files (*.trc *.nrd)")[0]
-        elif self.input_cbox_ncs.isChecked():
+        if ".nrd" in self.ext_ComboBox.currentText().lower() or ".trc" in self.ext_ComboBox.currentText().lower():
+            selected_path = QFileDialog.getOpenFileName(None, "Select an EEG file:", "EEG files (*.trc *.nrd)")[0]
+        elif ".ncs" in self.ext_ComboBox.currentText().lower():
             selected_path = QFileDialog.getExistingDirectory(None, 'Select a Neuralynx EEG folder', '~', QFileDialog.ShowDirsOnly)
 
         self.input_path_edit.setText(selected_path)
@@ -251,7 +278,7 @@ class empty_win(QWidget):
     def output_infoButton_fun(self):
         output_infoButton_dialog = QErrorMessage()
         output_infoButton_dialog.setWindowTitle("Output path information")
-        output_infoButton_dialog.showMessage('Search or paste the path to your BIDS-like folder.')            
+        output_infoButton_dialog.showMessage('Search or paste the path to your BIDS-like folder.')
         output_infoButton_dialog.exec()
 
     def output_browseButton_fun(self):
@@ -276,16 +303,17 @@ class empty_win(QWidget):
 
         ### Print BIDS path tree
         self.description_textbox.append("")
-        self.description_textbox.append("Original filepath:")
+        self.description_textbox.append("Original path:")
         self.description_textbox.append(self.input_path_edit.text())
-        self.description_textbox.append("BIDS-like filepath:")
+        self.description_textbox.append("")
+        self.description_textbox.append("BIDS-like root (destination):")
         self.description_textbox.append(self.output_path_edit.text())
 
         self.description_textbox.append("")
         self.description_textbox.append("BIDS-like tree:")
         tree_level = 1
         for _comp in path_components:
-            self.description_textbox.append("{} {}".format( "-"*tree_level,_comp ))
+            self.description_textbox.append("{} {}".format( "_"*tree_level,_comp ))
             tree_level+=2
 
     def button_OK_fun(self):
